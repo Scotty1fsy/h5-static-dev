5393cacd3435ca636ef3853b1c3594fc9ad3f786
jhh678 <jhh678878@163.com>
2018-11-02 00:58:38 +0800

    提交修改

--- src/js/pages/products.js
+++ src/js/pages/products.js
@@ -1,3 +1,6 @@
+var pageNo = 1;
+var pageSize = 10;
+
 $(function () {
   $('.classify-wrap .current-classify').finger('tap', function () {
     $('.classify-wrap ul').fadeToggle(100);
@@ -18,5 +21,35 @@ $(function () {
     $('.classify-wrap ul').fadeToggle(100);
     $('.current-classify .iconfont').toggleClass('icon-arrow-down');
     $('.current-classify .iconfont').toggleClass('icon-close');
+
+    getClassifyProducts(1);
+  })
+
+  $('.btn-more').finger('tap', function () {
+    getClassifyProducts(pageNo + 1);
   })
 });
+
+function getClassifyProducts(num) {
+  $.post('https://www.easy-mock.com/mock/5bcf2eb81b35885dcbeaaed1/solove/getClassifyProducts', {
+    id: $('.classify-wrap .current-classify').data('value'),
+    pageNo: num,
+    pageSize: pageSize
+  }, function (response) {
+    // process response
+    if (response.code === 200) {
+      var html = template('products', response.data.data);
+      if (num === 1) {
+        $('.products-wrap').html(html);
+      } else {
+        $('.products-wrap').append(html);
+      }
+
+      pageNo = num;
+
+      if (pageNo === Math.ceil(response.data.pagination.total / pageSize)) {
+        $('.btn-more').hide();
+      }
+    }
+  })
+}
